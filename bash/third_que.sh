f721e98df0420cf4f538ea51899949fc3fb0b54a
T-alpha17 <72997394+T-alpha17@users.noreply.github.com>
2024-08-20 03:31:49 -0700

    Delete bash directory

--- bash/third_que.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "1-minute CPU utilization: $(awk '{print $1}' /proc/loadavg)"
-echo "5-minute CPU utilization: $(awk '{print $2}' /proc/loadavg)"
-echo "15-minute CPU utilization: $(awk '{print $3}' /proc/loadavg)"
-
-echo "Total CPU utilization: $(top -b -n 1| awk '/Cpu/ {print $2}')%"
-echo "Total memory utilization: $(free -m | awk '/Mem/ {print $3 / $2 * 100 "%"}')"
-echo "Disk usage:" $(df / | grep / | awk '{print $5}')
